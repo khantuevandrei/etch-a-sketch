40a07e083b062f8005f10ef8c7aa9aecfbcf2d51
khantuevandrei <khantuevandrei@gmail.com>
2025-09-30 18:09:29 +0500

    Create getRandomColor() func to randomize an rgb value

--- script.js
+++ script.js
@@ -48,5 +48,10 @@ function makeNewGrid() {
 
 drawGrid();
 
-
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
 
